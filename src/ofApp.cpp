8e394957ef702e13c9f0a0fc11321d32662853b2
yiiiko <yikoli@Yiko.fios-router.home>
2019-04-13 19:36:26 -0400

    colortracking+particlesystem

---
+++ src/ofApp.cpp
@@ -0,0 +1,604 @@
+#include "ofApp.h"
+Params param;        //Definition of global variable
+//--------------------------------------------------------------
+
+
+void ofApp::setup(){
+    ofSetVerticalSync(true);
+    
+    ofSetCircleResolution(256);
+    ofBackground(255,255,255);
+    ofSetVerticalSync(true);
+    ofBackground(255,255,255);
+    ofSetFrameRate(60);
+    
+    //no change
+    w = 640;
+    h = 480;
+    
+    //webcam: select video input
+    vector<ofVideoDevice> devices = movie.listDevices();
+    for(int i = 0; i < devices.size(); i++){
+        if(devices[i].bAvailable){
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName;
+        }else{
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName << " - unavailable ";
+        }
+    }
+    
+    movie.setDeviceID(1);
+    movie.setDesiredFrameRate(60);
+    vidGrabber.initGrabber(w, h);
+    movie.initGrabber(w, h);
+    //reserve memory for cv images
+    rgb.allocate(w, h);
+    hsb.allocate(w, h);
+    hue.allocate(w, h);
+    sat.allocate(w, h);
+    bri.allocate(w, h);
+    filtered.allocate(w, h);
+    filter1.allocate(w, h);
+    filter2.allocate(w, h);
+    filter3.allocate(w, h);
+    
+    citizen.load("images/citizen.png");
+    citizenh.load("images/citizenh.png");
+    citizens.load("images/citizen.png");
+    
+//---------------------------------------------
+    
+    //Allocate drawing buffer
+    int w = ofGetWidth();
+    int h = ofGetHeight();
+    fbo.allocate( w, h, GL_RGB32F_ARB );
+    
+    //Fill buffer with white color
+    fbo.begin();
+    ofBackground(255, 255, 255);
+    fbo.end();
+    
+    //Set up parameters
+    param.setup();        //Global parameters
+    history = 0.0;
+    bornRate = 20;
+    
+    bornCount = 0;
+    time0 = ofGetElapsedTimef();
+    
+//    //GUI
+//    interf.setup();
+//    interf.addSlider( "rate", &bornRate, 0, 3000 );
+//    interf.addSlider( "lifeTime", &param.lifeTime, 0, 5 );
+//    interf.addSlider( "history", &history, 0, 1 );
+//
+//    interf.addSlider( "eRad", &param.eRad, 0, 800 );
+//    interf.addSlider( "velRad", &param.velRad, 0, 400 );
+//    interf.addSlider( "rotate", &param.rotate, -500, 500 );
+//    interf.addSlider( "spinning", &param.spinning, -1000, 1000 );
+//    interf.addSlider( "force", &param.force, -1000, 1000 );
+//    interf.addSlider( "friction", &param.friction, 0, 0.1 );
+//
+//    drawInterface = true;
+    
+}
+
+//--------------------------------------------------------------
+
+
+
+void ofApp::update(){
+    
+    
+    movie.update();
+    
+    if (movie.isFrameNew()) {
+        
+        //copy webcam pixels to rgb image
+        //        rgb.setFromPixels(movie.getPixels(), w, h);
+        rgb.setFromPixels(movie.getPixels());
+        //mirror horizontal
+        rgb.mirror(false, true);
+        
+        //duplicate rgb
+        hsb = rgb;
+        //convert to hsb
+        hsb.convertRgbToHsv();
+        //        rgb.convertHsvToRgb();
+        
+        //store the three channels as grayscale images
+        hsb.convertToGrayscalePlanarImages(hue, sat, bri);
+        
+        //        ofColor c = ofColor::fromHsb( 0, 255, 255 ); // bright red
+        //        c.setHue( 128 ); // now bright cyan
+        
+        
+        //filter image based on the hue value were looking for
+        //Goal is to track 3 different colors and draw different circles accordingly
+        
+        for (int i=0; i<w*h; i++) {
+            
+            filter1.getPixels()[i] = ofInRange(hue.getPixels()[i],trackHue1-1,trackHue1+1) ? 360:0; // trackHue1 = pink
+            filter2.getPixels()[i] = ofInRange(hue.getPixels()[i],trackHue2-1,trackHue2+1) ? 360:0; // trackHue2 = yellow
+            filter3.getPixels()[i] = ofInRange(hue.getPixels()[i],trackHue3-1,trackHue3+1) ? 360:0; // trackHue3 = blue
+        }
+        filter1.flagImageChanged();
+        filter2.flagImageChanged();
+        filter3.flagImageChanged();
+        
+        //run the contour finder on the filtered image to find blobs with a certain hue
+        contourFinder1.findContours(filter1, 50, w*h/4, 1, false, false); // pink
+        //        contourFinder2.findContours(filter2, 50, w*h/4, 1, false, false); // yellow
+        //        contourFinder3.findContours(filter3, 50, w*h/4, 1, false, false); // blue
+        
+        for (int i=0; i<contourFinder1.nBlobs; i++) {
+            param.eCenter1 = ofPoint(  contourFinder1.blobs[i].centroid.x+700, contourFinder1.blobs[i].centroid.y );
+        }
+        for (int i=0; i<contourFinder2.nBlobs; i++) {
+            param.eCenter2 = ofPoint(  contourFinder2.blobs[i].centroid.x+700, contourFinder2.blobs[i].centroid.y );
+        }
+        for (int i=0; i<contourFinder3.nBlobs; i++) {
+            param.eCenter3 = ofPoint(  contourFinder3.blobs[i].centroid.x+700, contourFinder3.blobs[i].centroid.y );
+        }
+    }
+    
+    //Compute dt
+    float time = ofGetElapsedTimef();
+    float dt = ofClamp( time - time0, 0, 0.1 );
+    time0 = time;
+    
+    //Delete inactive particles
+    int i=0;
+    while (i < p.size()) {
+        if ( !p[i].live ) {
+            p.erase( p.begin() + i );
+        }
+        else {
+            i++;
+        }
+    }
+    
+    //Born new particles
+    bornCount += dt * bornRate;      //Update bornCount value
+    if ( bornCount >= 1 ) {          //It's time to born particle(s)
+        int bornN = int( bornCount );//How many born
+        bornCount -= bornN;          //Correct bornCount value
+        for (int i=0; i<bornN; i++) {
+            Particle newP;
+            newP.setup();            //Start a new particle
+            p.push_back( newP );     //Add this particle to array
+        }
+    }
+    
+    //Update the particles
+    for (int i=0; i<p.size(); i++) {
+        p[i].update( dt );
+    }
+    
+}
+
+
+
+
+void Params::setup() {
+    
+    
+//    eCenter = ofPoint( ofGetWidth()/2, ofGetHeight()/2 );
+    eRad = 37;
+    velRad = 165;
+    lifeTime = 3.6;
+    rotate = 0;
+    force = -40;
+    spinning = 14;
+    friction = 0.01;
+}
+Particle::Particle() {
+    live = false;
+}
+
+ofPoint randomPointInCircle( float maxRad ){
+    ofPoint pnt;
+    float rad = ofRandom( 0, maxRad );
+    float angle = ofRandom( 0, M_TWO_PI );
+    pnt.x = cos( angle ) * rad;
+    pnt.y = sin( angle ) * rad;
+    return pnt;
+}
+
+void Particle::setup() {
+    pos = param.eCenter1 + randomPointInCircle( param.eRad );
+    vel = randomPointInCircle( param.velRad );
+    time = 0;
+    lifeTime = param.lifeTime;
+    live = true;
+}
+
+//--------------------------------------------------------------
+void Particle::update( float dt ){
+    if ( live ) {
+        //Rotate vel
+        vel.rotate( 0, 0, param.rotate * dt );
+        
+        ofPoint acc;         //Acceleration
+        ofPoint delta = pos - param.eCenter1;
+        float len = delta.length();
+        if ( ofInRange( len, 0, param.eRad ) ) {
+            delta.normalize();
+            
+            //Attraction/repulsion force
+            acc += delta * param.force;
+            
+            //Spinning force
+            acc.x += -delta.y * param.spinning;
+            acc.y += delta.x * param.spinning;
+        }
+        vel += acc * dt;            //Euler method
+        vel *= (1-param.friction);  //Friction
+        
+        //Update pos
+        pos += vel * dt;    //Euler method
+        
+        //Update time and check if particle should die
+        time += dt;
+        if ( time >= lifeTime ) {
+            live = false;   //Particle is now considered as died
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void Particle::draw(){
+    if ( live ) {
+        //Compute size
+        float size = ofMap(
+        fabs(time - lifeTime/2), 0, lifeTime/2, 3, 1 );
+        
+        //Compute color
+        //        ofColor color = ofColor::black;
+        float color = ofMap( time, 0, lifeTime, 100, 255 );
+        ofSetColor( color );
+        
+        ofDrawCircle( pos, size );  //Draw particle
+    }
+}
+
+//--------------------------------------------------------------
+
+
+void ofApp::draw(){
+    
+  
+    
+  
+    
+    //1. Drawing to buffer
+    fbo.begin();
+    
+    //Draw semi-transparent white rectangle
+    //to slightly clearing a buffer (depends on history value)
+    
+    ofEnableAlphaBlending();         //Enable transparency
+    
+    float alpha = (1-history) * 255;
+    ofSetColor( 255, 255, 255, alpha );
+    ofFill();
+    ofDrawRectangle( 0, 0, ofGetWidth(), ofGetHeight() );
+    
+    ofDisableAlphaBlending();        //Disable transparency
+    
+    
+    //floating particles
+    
+    ofSetColor(230);
+    ofNoFill();
+    
+    
+    for (int i = 0; i < 6; i++){
+        ofDrawRectangle(700+(i*105),50,100,100);
+        ofDrawRectangle(700+(i*105),155,100,100);
+        ofDrawRectangle(700+(i*105),260,100,100);
+        ofDrawRectangle(700+(i*105),365,100,100);
+        ofDrawRectangle(700+(i*105),470,100,100);
+        
+    }
+    for (int j = 0; j < 5; j++){
+        ofDrawRectangle(700,50+(j*105),100,100);
+    }
+    
+    ofSetColor(190);
+    ofSetHexColor(0x000000);
+    ofNoFill();
+    ofBeginShape();
+    for (int i = 0; i < nPts; i++){
+        ofVertex(pts[i].x, pts[i].y);
+    }
+    ofSetColor(255,255,255);
+    
+    //draw all cv images
+    rgb.draw(0,0);
+    //    hsb.draw(640,0);
+    //    hue.draw(0,480);
+    //    sat.draw(640,480);
+    //    bri.draw(1280,480);
+    //    filtered.draw(0,960);
+    //    contours.draw(0,0);
+    
+    ofSetColor(0, 0, 230);
+    ofFill();
+    
+    
+    //Draw the particles
+    ofFill();
+    for (int i=0; i<p.size(); i++) {
+        p[i].draw();
+    }
+    
+    fbo.end();
+    
+    //2. Draw buffer on the screen
+    ofSetColor( 255, 255, 255 );
+    fbo.draw( 0, 0 );
+    
+    //GUI
+//    if ( drawInterface ) {
+//        //        //Draw text
+//        //        ofSetColor( 0, 0, 0 );
+//        //        ofDrawBitmapString( "Particles: " + ofToString( p.size() ), 20, 40 );
+//        //
+//        //        //Draw sliders
+////                interf.draw();
+////
+//        //Draw emitter as a circle
+//        ofSetCircleResolution( 50 );
+//        ofNoFill();
+//        ofSetColor( 128, 128, 128 );
+//        ofDrawCircle( param.eCenter, param.eRad );
+//        ofSetCircleResolution( 20 );
+//    }
+    
+    
+    
+    float x3 = 850;
+    float y3 = 200;
+    
+    //draw blue circles for found blobs
+    for (int i=0; i<contourFinder1.nBlobs; i++) {
+        float x = contourFinder1.blobs[i].centroid.x+100*cos(ofGetElapsedTimef()/3.0f);
+        float y = contourFinder1.blobs[i].centroid.y+100*sin(ofGetElapsedTimef()/2.5f);
+        float x0 = contourFinder1.blobs[i].centroid.x+100*cos(ofGetElapsedTimef()/1.5f);
+        float y0 = contourFinder1.blobs[i].centroid.y+100*sin(ofGetElapsedTimef()/2.5f);
+        ofSetColor(0, 0, 255);
+        ofDrawCircle(contourFinder1.blobs[i].centroid.x, contourFinder1.blobs[i].centroid.y, 10); // drawing blue circles on top of video
+        ofSetColor(255, 153, 170);
+        ofDrawCircle(contourFinder1.blobs[i].centroid.x+700, contourFinder1.blobs[i].centroid.y, 30); // drawing pink circles on grid
+        ofSetColor(183, 73, 91);
+        ofDrawBitmapString("wind turbine", contourFinder1.blobs[i].centroid.x+650, contourFinder1.blobs[i].centroid.y+50);
+        citizen.draw(x0+700,y0,30,30);
+        citizen.draw(x0,y0,30,30);
+        citizen.draw(x,y,30,30);
+        
+    }
+    
+//    for (int i=0; i<contourFinder2.nBlobs; i++) {
+//        float x1 = contourFinder2.blobs[i].centroid.x+50*cos(ofGetElapsedTimef()*1.0f);
+//        float y1 = contourFinder2.blobs[i].centroid.y+100*sin(ofGetElapsedTimef()/3.5f);
+//        ofSetColor(0, 0, 255);
+//        ofDrawCircle(contourFinder2.blobs[i].centroid.x, contourFinder2.blobs[i].centroid.y, 30); // drawing blue circles on top of video
+//        ofSetColor(244, 215, 100);
+//        ofDrawCircle(contourFinder2.blobs[i].centroid.x+700, contourFinder2.blobs[i].centroid.y, 30); // drawing pink circles on grid
+//        ofSetColor(191, 115, 22);
+//        ofDrawBitmapString("solar station", contourFinder2.blobs[i].centroid.x+650, contourFinder2.blobs[i].centroid.y+50);
+//        citizen.draw(x1+700, y1,30,30);
+//    }
+    
+    //    for (int i=0; i<contourFinder3.nBlobs; i++) {
+    //        float x2 = contourFinder3.blobs[i].centroid.x+30*cos(ofGetElapsedTimef()*2.0f);
+    //        float y2 = contourFinder3.blobs[i].centroid.y+100*sin(ofGetElapsedTimef()*0.75f);
+    //        ofSetColor(0, 0, 255);
+    //        ofDrawCircle(contourFinder3.blobs[i].centroid.x, contourFinder3.blobs[i].centroid.y, 30); // drawing blue circles on top of video
+    //        ofSetColor(100, 143, 244);
+    //        ofDrawCircle(contourFinder3.blobs[i].centroid.x+700, contourFinder3.blobs[i].centroid.y, 30); // drawing pink circles on grid
+    //        ofSetColor(9, 37, 104);
+    //        ofDrawBitmapString("nuclear plant", contourFinder3.blobs[i].centroid.x+650, contourFinder3.blobs[i].centroid.y+50);
+    //        citizen.draw(x2+700, y2,30,30);
+    //    }
+    
+    ofSetColor(0, 0, 230);
+    //    ofSetHexColor(0x000000);
+    ofNoFill();
+    ofBeginShape();
+    ofEndShape();
+    
+    
+   
+
+    
+    
+    
+    //print data
+    ofSetColor(107, 114, 124);
+    stringstream reportStream;
+    reportStream << "Pink hue value " << trackHue1 << "    " << "pink blobs found " << contourFinder1.nBlobs   <<endl
+    << "Yellow hue value " << trackHue2 << "  " << "yellow blobs found " << contourFinder2.nBlobs <<endl
+    << "Blue hue value " << trackHue3 << "    " << "blue blobs found " << contourFinder3.nBlobs  <<endl;
+    ofDrawBitmapString(reportStream.str(), 20, 550);
+    
+    //data monitor
+    ofSetColor(51, 57, 66);
+    stringstream townData;
+    townData<< "Town size: " << (contourFinder1 .nBlobs*3) + (contourFinder2.nBlobs*4) + (contourFinder3.nBlobs*8)<< " people" << endl
+    << "Town capita: $" << ((contourFinder1 .nBlobs*3) + (contourFinder2.nBlobs*4) + (contourFinder3.nBlobs*8))*1.77 <<endl
+    << "Public health score: " << (contourFinder1 .nBlobs*0.43) + (contourFinder2.nBlobs*0.8) + (contourFinder3.nBlobs*3.75) << " pts" <<endl;
+    //    << "Air pollutant level:" << trackHue1 <<endl;
+    ofDrawBitmapString(townData.str(), 670, 30);
+    
+    //---------------------------------------------------------
+    
+}
+
+
+//--------------------------------------------------------------
+//----------------------  GUI ----------------------------------
+//--------------------------------------------------------------
+void Interface::setup(){
+    selected = -1;
+}
+
+void Interface::addSlider( string title, float *value, float minV, float maxV ){
+    Slider s;
+    s.title = title;
+    s.rect = ofRectangle( 20, 60 + slider.size() * 40, 150, 30 );
+    s.value = value;
+    s.minV = minV;
+    s.maxV = maxV;
+    slider.push_back( s );
+}
+
+void Interface::draw(){
+    for (int i=0; i<slider.size(); i++) {
+        Slider &s = slider[i];
+        ofRectangle r = s.rect;
+        ofFill();
+        ofSetColor( 255, 255, 255 );
+        ofDrawRectangle( r );
+        ofSetColor( 0, 0, 0 );
+        ofNoFill();
+        ofDrawRectangle( r );
+        ofFill();
+        float w = ofMap( *s.value, s.minV, s.maxV, 0, r.width );
+        ofDrawRectangle( r.x, r.y + 15, w, r.height - 15 );
+        ofDrawBitmapString( s.title + " " + ofToString( *s.value, 2 ), r.x + 5, r.y + 12 );
+    }
+}
+
+void Interface::mousePressed( int x, int y ){
+    for (int i=0; i<slider.size(); i++) {
+        Slider &s = slider[i];
+        ofRectangle r = s.rect;
+        if ( ofInRange( x, r.x, r.x + r.width ) && ofInRange( y, r.y, r.y + r.height ) ) {
+            selected = i;
+            *s.value = ofMap( x, r.x, r.x + r.width, s.minV, s.maxV, true );
+        }
+    }
+}
+
+void Interface::mouseDragged( int x, int y ){
+    if ( selected >= 0 ) {
+        Slider &s = slider[selected];
+        ofRectangle r = s.rect;
+        *s.value = ofMap( x, r.x, r.x + r.width, s.minV, s.maxV, true );
+    }
+}
+
+void Interface::mouseReleased (int x, int y ){
+    selected = -1;
+}
+
+//--------------------------------------------------------------
+//For saving/loading presets we use very simple method:
+//just pack all sliders values into list and save/load it from file.
+//Its very simple, but is not practical, because when saved,
+//you can not change the number of sliders and these order.
+//The best solution is using ofxXmlSettings - it lets write sliders values
+//and specify these names
+
+void Interface::save( int index )
+{
+    vector<string> list;
+    for (int i=0; i<slider.size(); i++) {
+        list.push_back( ofToString( *slider[i].value ) );
+    }
+    string text = ofJoinString( list," " );
+    string fileName = "preset" + ofToString( index ) + ".txt";
+    ofBuffer buffer = ofBuffer( text );
+    ofBufferToFile( fileName, buffer );
+}
+
+//--------------------------------------------------------------
+void Interface::load( int index )
+{
+    string fileName = "preset" + ofToString( index ) + ".txt";
+    string text = string( ofBufferFromFile( fileName ) );
+    vector<string> list = ofSplitString( text, " " );
+    
+    if ( list.size() == slider.size() ) {
+        for (int i=0; i<slider.size(); i++) {
+            *slider[i].value = ofToFloat( list[i] );
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+    if (key == '1'){
+        int mx = mouseX % w;
+        int my = mouseY % h;
+        trackHue1 = hue.getPixels()[my*w+mx];
+    }
+    
+    if (key == '2'){
+        int mx = mouseX % w;
+        int my = mouseY % h;
+        trackHue2 = hue.getPixels()[my*w+mx];
+    }
+    
+    if (key == '3'){
+        int mx = mouseX % w;
+        int my = mouseY % h;
+        trackHue3 = hue.getPixels()[my*w+mx];
+    }
+    
+}
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    if ( drawInterface ) {
+        interf.mouseDragged( x, y );
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+    if ( drawInterface ) {
+        interf.mousePressed( x, y );
+    }
+}
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
+
---
+++ src/ofApp.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#define MAX_N_PTS         1500
+
+
+//Particle class
+class Particle {
+public:
+    Particle();                //Class constructor
+    void setup();              //Start particle
+    void update( float dt );   //Recalculate physics
+    void draw();               //Draw particle
+    
+    ofPoint pos;               //Position
+    ofPoint vel;               //Velocity
+    float time;                //Time of living
+    float lifeTime;            //Allowed lifetime
+    bool live;                 //Is particle live
+};
+
+//Control parameters class
+class Params {
+public:
+    void setup();
+    ofPoint eCenter1;    //Emitter center
+    ofPoint eCenter2;    //Emitter center
+    ofPoint eCenter3;    //Emitter center
+    float eRad;         //Emitter radius
+    float velRad;       //Initial velocity limit
+    float lifeTime;     //Lifetime in seconds
+    
+    float rotate;   //Direction rotation speed in angles per second
+    
+    float force;       //Attraction/repulsion force inside emitter
+    float spinning;    //Spinning force inside emitter
+    float friction;    //Friction, in the range [0, 1]
+    float transparency;
+    ofxCvContourFinder contourFinder1;
+    ofxCvContourFinder contourFinder2;
+    ofxCvContourFinder contourFinder3;
+    
+};
+
+extern Params params; //Declaration of a global variable
+
+
+//------------ GUI --------------
+//Slider class
+class Slider {
+public:
+    string title;        //Title
+    ofRectangle rect;    //Rectangle for drawing
+    float *value;       //Pointer to value which the slider changes
+    float minV, maxV;   //Minimum and maximum values
+};
+
+//Interface class, which manages sliders
+class Interface {
+public:
+    void setup();
+    void addSlider( string title, float *value, float minV, float maxV );
+    void draw();
+    
+    void save( int index );        //Save preset
+    void load( int index );        //Load preset
+    
+    void mousePressed( int x, int y );
+    void mouseDragged( int x, int y );
+    void mouseReleased (int x, int y );
+    
+    vector<Slider> slider;    //Array of sliders
+    int selected;            //Index of selected slider
+};
+
+typedef struct {
+    
+    float     x;
+    float     y;
+    bool     bBeingDragged;
+    bool     bOver;
+    float     radius;
+    
+}draggableVertex;
+
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void resetParticles();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    ofVec3f    pts[MAX_N_PTS];
+    int        nPts;
+    int        r1;
+    int        r2;
+    int        r3;
+    ofPoint eCenter;    //Emitter center
+    ofPoint pos;
+    ofPoint vel;
+    ofPoint frc;
+    
+    vector<Particle> p;      //Particles
+    ofFbo fbo;            //Off-screen buffer for trails
+    
+    float history;        //Control parameter for trails
+    float time0;          //Time value for computing dt
+    
+    float bornRate;       //Particles born rate per second
+    float bornCount;      //Integrated number of particles to born
+    
+    //GUI
+    Interface interf;
+    bool drawInterface;
+    
+    
+    bool bDrawnAnything;
+    
+    ofVideoGrabber movie;
+    ofxCvColorImage rgb,hsb;
+    ofxCvGrayscaleImage hue,sat,bri,filtered,filter1,filter2,filter3;
+    ofxCvContourFinder contourFinder1;
+    ofxCvContourFinder contourFinder2;
+    ofxCvContourFinder contourFinder3;
+    
+    ofImage citizen;
+    ofImage citizenh;
+    ofImage citizens;
+    
+    
+    ofVideoGrabber vidGrabber;
+    int camWidth;
+    int camHeight;
+    int w,h;
+    int trackHue1;
+    int trackHue2;
+    int trackHue3;
+    
+    
+    void drawNoisyArmRect (float w, float h);
+    
+    
+    string currentModeStr;
+    vector <ofPoint> attractPoints;
+    vector <ofPoint> attractPointsWithMovement;
+    
+};
+
